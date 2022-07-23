e131fc8e9da717da7fb42f9eff4585b30a3a9943
Lucas <lucasvalenteds@gmail.com>
2022-07-23 16:20:48 -0300

    Adding Gradle build script

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("org.springframework.boot") version "3.0.0-M4"
+    id("io.spring.dependency-management") version "1.0.12.RELEASE"
+    java
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(18))
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven(url = "https://repo.spring.io/milestone")
+}
+
+ext {
+    set("testcontainersVersion", "1.17.3")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.flywaydb:flyway-core")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webflux")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.testcontainers:testcontainers-bom:${ext["testcontainersVersion"]}")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
